030ae5bc22daa5959783f83c7d155ff378d20a97
AKruKovych <anatoliinakrukovich@gmail.com>
2020-02-05 22:35:21 +0200

    db,parser and bot are done version 1.0

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ def get_connection():
     global __connection
 
     if not __connection:
-        __connection = sqlite3.connect('bot_db.db',check_same_thread=False)
+        __connection = sqlite3.connect('bot_db.db', check_same_thread=False)
 
     return __connection
 
@@ -18,32 +18,88 @@ def init_db(force: bool = None):
     cursor = conn.cursor()
 
     if force:
-        cursor.execute('DROP TABLE IF EXISTS user_message')
+        cursor.execute('DROP TABLE IF EXISTS users')
+        cursor.execute('DROP TABLE IF EXISTS shows')
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS user_message(
-         id          INTEGER PRIMARY KEY,
-         user_id     INTEGER NOT NULL,
-         text        TEXT NOT NULL
+    CREATE TABLE IF NOT EXISTS users(
+         user_id     INTEGER PRIMARY KEY,
+         first_name        TEXT NOT NULL
          ) 
          """)
 
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS shows(
+             id          INTEGER PRIMARY KEY,
+             user_id          INTEGER NOT NULL,
+             title          TEXT NOT NULL,
+             season     INTEGER NOT NULL,
+             episode        INTEGER NOT NULL,
+             FOREIGN KEY(user_id) REFERENCES users(user_id)
+             ) 
+             """)
+
     conn.commit()
 
 
-def add_message(user_id: int, text: str):
+def add_user(user_id: int, first_name: str):
     conn = get_connection()
     c = conn.cursor()
-    c.execute('INSERT INTO user_message (user_id,text) VALUES  (?,?)', (user_id, text))
+    c.execute('Select * from users where user_id = ?', (user_id,))
+    user = c.fetchone()
+
+    if not user:
+
+        c.execute('INSERT INTO users(user_id,first_name) VALUES  (?,?)', (user_id, first_name))
+        conn.commit()
+
+    else:
+        pass
+
+
+def add_show(title: str, user_id: int, season: int, episode: int):
+    title = title.title()
+    conn = get_connection()
+    c = conn.cursor()
+    c.execute('Select * FROM shows WHERE title = ? and user_id = ?; ', (title, user_id))
+    data = c.fetchone()
+    if not data:
+
+        c.execute('INSERT INTO shows(title, user_id,season,episode) VALUES  (?,?,?,?)',
+                  (title, user_id, season, episode))
+        conn.commit()
+    else:
+        pass
+
+
+def update_show(title: str, user_id: int, season: int, episode: int):
+    title = title.title()
+    conn = get_connection()
+    c = conn.cursor()
+    c.execute('UPDATE shows set season = ?,episode = ? WHERE user_id = ? and title = ?',
+              (season, episode, user_id, title))
     conn.commit()
 
 
-def get_info():
+def delete_show(title: str, user_id: int):
+    title = title.title()
     conn = get_connection()
     c = conn.cursor()
-    c.execute('Select * from user_message')
-    data = c.fetchall()
+    c.execute('Select * From shows where title = ?;', (title,))
+    shows = c.fetchall()
+
+    if not shows:
+        raise ValueError
+
+    c.execute('DELETE FROM shows WHERE user_id = ? and title = ? ;',
+              (user_id, title))
+    conn.commit()
+
+
+def get_all_shows(user_id: int):
+    conn = get_connection()
+    c = conn.cursor()
+    data = c.execute('Select * From shows where user_id = ?;', (user_id,))
+    data = data.fetchall()
 
     return data
-
-init_db(True)
---
+++ tele_bot.py
@@ -0,0 +1,178 @@
+import telebot, types
+from telebot import types
+import os
+import logging
+import parser
+import db
+
+
+parser = parser.ImdbParser()
+
+logger = telebot.logger
+
+telebot.logger.setLevel(logging.WARNING)
+TOKEN = '856121961:AAHax7gKb-iDJ9Pl-nNtZKV5VgrEjg5QcNE'
+
+bot = telebot.TeleBot(TOKEN)
+
+answers = ('Random 5 movies to watch', 'Add/Update/Delete/All a list of tv shows')
+
+
+@bot.message_handler(commands=['start', 'help'])
+@bot.edited_message_handler(commands=['start', 'help'])
+def command_handler(message: types.Message):
+
+    if message.text == '/start':
+        db.add_user(message.from_user.id, message.from_user.first_name)
+
+        bot.send_sticker(message.chat.id,
+                         'CAACAgEAAxkBAAMaXjnCIjuhV6-wpLsKtDRksUHERd8AAn0AA8WInATR-Nr5m7ajfxgE'
+                         )
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton(answers[0])
+        item2 = types.KeyboardButton(answers[1])
+
+        markup.add(item1, item2)
+        bot.send_message(message.chat.id, 'Evil Morty bot wants to have some fun ...', reply_markup=markup)
+
+    else:
+        bot.send_message(message.chat.id, """If you don't know what you want to watch then pick on Random 5 movies to watch.
+But if you need update your tv shows data then the right button is your choice.""" )
+
+
+@bot.message_handler(content_types=['text'])
+def reply_on_markup(message: types.Message):
+
+    if message.text == answers[0]:
+        reply = parser.get_movies_string(True)
+        bot.send_message(message.chat.id, reply)
+
+    elif message.text == answers[1]:
+        murkup = types.InlineKeyboardMarkup(row_width=4)
+
+        item1 = types.InlineKeyboardButton('Add tv show', callback_data='add')
+        item2 = types.InlineKeyboardButton('Update', callback_data='update')
+        item3 = types.InlineKeyboardButton('Delete', callback_data='delete')
+        item4 = types.InlineKeyboardButton('All', callback_data='all')
+
+        murkup.add(item1, item2, item3, item4)
+
+        bot.send_message(message.chat.id, "Don't be like Rick ... ", reply_markup=murkup)
+
+    else:
+        bot.send_message(message.chat.id, 'I dunno bro, this looks like kek ...')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def add_show(call):
+    try:
+        if call.message:
+
+            if call.data == 'add':
+                msg = bot.send_message(call.message.chat.id,
+                                       'add your tv show in this pattern "title-season-episode" .')
+                bot.register_next_step_handler(msg, add_show)
+
+            elif call.data == 'delete':
+                msg = bot.send_message(call.message.chat.id, 'delete your tv show in this pattern "title" .')
+                bot.register_next_step_handler(msg, delete_show)
+
+            elif call.data == 'update':
+                msg = bot.send_message(call.message.chat.id,
+                                       'update your tv show in this pattern "title-season-episode" .')
+                bot.register_next_step_handler(msg, update_show)
+
+            elif call.data == 'all':
+                reply = get_all_shows(call.from_user.id)
+                bot.send_message(call.message.chat.id, reply, parse_mode='html')
+
+    except Exception as e:
+        bot.send_message(call.message.chat.id, e)
+
+
+@bot.message_handler(content_types=['sticker'])
+def say(message: types.Message):
+
+    bot.send_sticker(message.chat.id, data='CAACAgEAAxkBAAMaXjnCIjuhV6-wpLsKtDRksUHERd8AAn0AA8WInATR-Nr5m7ajfxgE')
+
+
+@bot.message_handler(content_types=['text'])
+@bot.edited_message_handler(content_types=['text'])
+def add_show(message: types.Message):
+
+    reply = ''
+    try:
+        data = message.text.split('-')
+
+        if len(data) > 3:
+            raise ValueError
+
+        db.add_show(data[0], message.from_user.id, data[1], data[2])
+
+        reply =  f'Added {data[0]} ... '
+
+    except Exception as e:
+        reply = f'Not valid format! must bee title-season-episode'
+
+    finally:
+        bot.send_message(message.chat.id, reply)
+
+
+@bot.message_handler(content_types=['text'])
+@bot.edited_message_handler(content_types=['text'])
+def update_show(message: types.Message):
+
+    reply = ''
+
+    try:
+        data = message.text.split('-')
+
+        if len(data) > 3:
+            raise ValueError
+
+        db.update_show(data[0], message.from_user.id, data[1], data[2])
+
+        reply = f'Updated {data[0]} ... '
+
+    except Exception as e:
+        reply = f'Not valid format! must bee title-season-episode'
+
+    finally:
+        bot.send_message(message.chat.id, reply)
+
+
+@bot.message_handler(content_types=['text'])
+@bot.edited_message_handler(content_types=['text'])
+def delete_show(message: types.Message):
+
+    reply = ''
+    data = message.text.strip()
+    try:
+        db.delete_show(data, message.from_user.id)
+        reply = f'Deleted {data}'
+
+    except Exception as e:
+        reply = 'Not found or invalid format!'
+
+    finally:
+        bot.send_message(message.chat.id, reply)
+
+
+def get_all_shows(user_id):
+
+    reply = ''
+    shows = db.get_all_shows(user_id)
+
+    if not shows:
+        reply = 'I am scared! It\'s empty.'
+
+    else:
+        for show in shows:
+            string = f'''Show - {show[2]}\nSeason - {show[3]}\nEpisode - {show[4]}\n\n'''
+            reply = ''.join([reply, string])
+
+    return reply
+
+
+bot.polling()
