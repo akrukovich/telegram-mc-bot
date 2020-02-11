bf76922bd6d1176ae504607f36c84f93f2987fcf
akrukovich <anatoliinakrukovich@example.com>
2020-02-11 16:45:43 +0200

    multiprocessing added

--- parser.py
+++ parser.py
@@ -56,66 +56,51 @@ class ImdbParser:
 
         return req_contents
 
+    @staticmethod
+    def read_content(rt):
+
+        soup = BeautifulSoup(rt.content, 'lxml')
+        data = soup.find_all('div', class_='lister-item mode-advanced')
+        for i in data:
+            name = i.h3.a.text
+            link = ''.join(('https://www.imdb.com', i.h3.a['href']))
+            year = i.find('span', class_='lister-item-year text-muted unbold').text[1:-1]
+            rating = i.find('div', class_='inline-block ratings-imdb-rating').text.strip()
+            genre = i.find('span', class_='genre').text.strip()
+            cast = i.find_all('p', class_='')
+            stars = list()
+            for actor in cast[0].find_all('a'):
+                stars.append(actor.text)
+            director, *actors = stars
+
+            d = {'name': name,
+                 'link': link,
+                 'year': year,
+                 'rating': rating,
+                 'genre': genre,
+                 'director': director,
+                 'actors': actors}
+
+            return d
+
     def get_info(self, select: bool = False, amount: int = 5):
-        """
-        Function for parsed data handling from imdb.com.
 
-        Function parse name link, year, rating, genre and cast of movie.
-        Args:
-            select: Boolean value indicates random movies selection
-            amount: Amount of choices for random.
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            rts = self.__get_requests()
 
-        Returns:
-            info: A list of parsed info.
-        """
+            results = (executor.submit(read_content, r) for r in rts)
 
-        info = []
-
-        for request in self.__get_requests():
-
-            soup = BeautifulSoup(request.content, 'lxml')
-
-            data = soup.find_all('div', class_='lister-item mode-advanced')
-            for i in data:
-                name = i.h3.a.text
-                link = ''.join(('https://www.imdb.com', i.h3.a['href']))
-                year = i.find('span', class_='lister-item-year text-muted unbold').text[1:-1]
-                rating = i.find('div', class_='inline-block ratings-imdb-rating').text.strip()
-                genre = i.find('span', class_='genre').text.strip()
-                cast = i.find_all('p', class_='')
-
-                stars = list()
-
-                for actor in cast[0].find_all('a'):
-                    stars.append(actor.text)
-
-                director, *actors = stars
-
-                #
-                # cast = (actor.find_all('a')[0].text for actor in cast)
-
-                # print(next(cast))
-
-                # for actor in actors:
-                #     [print(i.text) for i in actor.find_all('a')]
-
-                info.append(
-                    {'name': name,
-                     'link': link,
-                     'year': year,
-                     'rating': rating,
-                     'genre': genre,
-                     'director': director,
-                     'actors': actors}
-                )
+            movies = (movie.result() for movie in concurrent.futures.as_completed(results))
+            movies = list(movies)
 
         if select:
+
             import random
-            info = random.sample(info, k=amount)
 
-            return info
+            movies = random.sample(movies, k=amount)
+            return movies
 
-        return info
+        return movies
 
     def get_movies_string(self, select=True):
 
